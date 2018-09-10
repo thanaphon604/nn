442a4e913722d6a19b11c80b7a6148d36bfdddc8
thanaphon604 <poper.604@gmail.com>
2018-09-10 20:24:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express  = require('express')
+const mongodb = require('mongodb')
+
+var app = express() //เรียกใช้เสมอถ้าใช้ express
+
+app.get( '/',(req,res) =>{
+  res.send('hello')
+})
+
+app.listen(3000,() =>{
+    console.log('is listen on port 3000')
+})
