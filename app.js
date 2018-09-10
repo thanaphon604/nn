1781b754d85872f5b90f428d6e714a04857a1591
thanaphon604 <poper.604@gmail.com>
2018-09-10 21:47:06 +0700

    app.get student query by name ,id

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const express  = require('express')
+const express = require('express')
 const mongodb = require('mongodb')
 
 var app = express() //เรียกใช้เสมอถ้าใช้ express
 
-app.get( '/',(req,res) =>{
-  res.send('hello')
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+let student = [
+    {
+        id: 'st1',
+        name: 'p1'
+    }, {
+        id: 'st2',
+        name: 'p2'
+    }, {
+        id: 'st3',
+        name: 'p3'
+    }
+]
+app.get('/all-student', (req, res) => {
+    res.send(student)
 })
 
-app.listen(3000,() =>{
+app.get('/all-student/:id', (req, res) => {
+    let studentID = req.params.id
+    for (let i = 0; i< student.length; i++) {
+        if (studentID == student[i].id) {
+            res.send(student[i])
+            break;
+        }
+    }
+    res.send('not found this id :' + studentID)
+})
+
+app.get('/all-student/name/:name', (req, res) => {
+    let studentNAME = req.params.name
+    for (let i = 0; i< student.length; i++) {
+        if (studentNAME == student[i].name) {
+            res.send(student[i])
+            break;
+        }
+    }
+    res.send('not found this name :' + studentNAME)
+})
+
+
+app.listen(3000, () => {
     console.log('is listen on port 3000')
 })
